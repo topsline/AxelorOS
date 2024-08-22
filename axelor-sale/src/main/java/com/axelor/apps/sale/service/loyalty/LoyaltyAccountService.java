60b5253c1949fff677e9ddf4922df4a2eda9c007
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-08-22 14:28:15 +0200

    RM#82617: Loyalty Account : points earning batch

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountService.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountService.java
@@ -3,8 +3,11 @@ package com.axelor.apps.sale.service.loyalty;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.sale.db.LoyaltyAccount;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface LoyaltyAccountService {
   Optional<LoyaltyAccount> getLoyaltyAccount(Partner partner, Company company);
+
+  LoyaltyAccount acquirePoints(LoyaltyAccount loyaltyAccount, LocalDateTime limitDateTime);
 }
--- axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountServiceImpl.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountServiceImpl.java
@@ -3,6 +3,9 @@ package com.axelor.apps.sale.service.loyalty;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.sale.db.LoyaltyAccount;
+import com.google.inject.persist.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class LoyaltyAccountServiceImpl implements LoyaltyAccountService {
@@ -16,6 +19,26 @@ public class LoyaltyAccountServiceImpl implements LoyaltyAccountService {
               .filter(account -> company.equals(account.getCompany()))
               .findFirst();
     }
+    return loyaltyAccount;
+  }
+
+  @Override
+  @Transactional(rollbackOn = {Exception.class})
+  public LoyaltyAccount acquirePoints(LoyaltyAccount loyaltyAccount, LocalDateTime limitDateTime) {
+    loyaltyAccount.getHistoryLineList().stream()
+        .filter(
+            historyLine ->
+                !historyLine.getPointsAcquired()
+                    && historyLine.getSaleOrder().getConfirmationDate().isAfter(limitDateTime))
+        .forEach(
+            historyLine -> {
+              BigDecimal pointsBalance = historyLine.getPointsBalance();
+              historyLine.setRemainingPoints(pointsBalance);
+              historyLine.setAcquisitionDateTime(LocalDateTime.now());
+              historyLine.setPointsAcquired(true);
+              loyaltyAccount.setPointsBalance(
+                  loyaltyAccount.getPointsBalance().add(historyLine.getPointsBalance()));
+            });
     return loyaltyAccount;
   }
 }
