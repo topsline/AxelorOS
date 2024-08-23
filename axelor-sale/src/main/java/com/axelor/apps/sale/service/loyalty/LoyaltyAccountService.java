073ddbebebb1aef955c15654714421f600cbc78f
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-08-23 16:40:55 +0200

    RM#82617: Loyalty Account : points earning batch

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountService.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/loyalty/LoyaltyAccountService.java
@@ -3,11 +3,10 @@ package com.axelor.apps.sale.service.loyalty;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.sale.db.LoyaltyAccount;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface LoyaltyAccountService {
   Optional<LoyaltyAccount> getLoyaltyAccount(Partner partner, Company company);
 
-  LoyaltyAccount acquirePoints(LoyaltyAccount loyaltyAccount, LocalDateTime limitDateTime);
+  LoyaltyAccount acquirePoints(LoyaltyAccount loyaltyAccount, Integer delay);
 }
